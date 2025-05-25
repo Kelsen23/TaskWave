11b39483866e9f664c004bf307ced1d4d5a63a71
Kelsen23 <nikolozigoreziani@gmail.com>
2025-05-25 22:53:25 +0400

    Move TaskManager code that was supposed to be in App in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
+import { Route, Routes } from "react-router-dom";
+import PrivateRoute from "./components/PrivateRoute";
+import TaskManager from "./components/TaskManager";
+import Authentication from "./components/Authentication";
 
 const App = () => {
   return (
     <div>
-      
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <PrivateRoute>
+              <TaskManager />
+            </PrivateRoute>
+          }
+        />
+        <Route path="/auth" element={<Authentication />} />
+      </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
