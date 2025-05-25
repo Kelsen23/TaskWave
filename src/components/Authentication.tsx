e444f940ddca44b61ca1da3ede5245ab2066d0d6
Kelsen23 <nikolozigoreziani@gmail.com>
2025-05-25 23:01:09 +0400

    Setup the SignUp and LogIn componets in Authentication

--- src/components/Authentication.tsx
+++ src/components/Authentication.tsx
@@ -1,10 +1,16 @@
+import LogIn from "./LogIn";
+import SignUp from "./SignUp";
 
 const Authentication = () => {
   return (
-    <div>
-      
+    <div className="flex h-screen justify-center items-center">
+      <div className="flex items-center gap-30">
+        <SignUp />
+        <div className="text-gray-500 font-semibold mx-4">OR</div>
+        <LogIn />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Authentication
+export default Authentication;
