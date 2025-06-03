10f04aca6f0d7b5a9c471fce47a96480397baf00
Kelsen23 <nikolozigoreziani@gmail.com>
2025-06-03 11:45:51 +0400

    Make SignUp component with UI and functionality

--- src/components/Authentication.tsx
+++ src/components/Authentication.tsx
@@ -1,13 +1,16 @@
+import { useState } from "react";
 import LogIn from "./LogIn";
 import SignUp from "./SignUp";
 
 const Authentication = () => {
+  const [loggedIn, setLoggedIn] = useState(false);
+
   return (
     <div className="flex h-screen justify-center items-center">
-      <div className="flex items-center gap-30">
-        <SignUp />
+      <div className="flex items-center gap-60">
+        <SignUp loggedIn={loggedIn} setLoggedIn={setLoggedIn} />
         <div className="text-gray-500 font-semibold mx-4">OR</div>
-        <LogIn />
+        <LogIn loggedIn={loggedIn} setLoggedIn={setLoggedIn} />
       </div>
     </div>
   );
