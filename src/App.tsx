d2cd157887de26df236397f22b69755bf650fa75
Kelsen23 <nikolozigoreziani@gmail.com>
2025-06-16 15:28:10 +0400

    Save logging in using side effects in App

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,16 @@ import { Route, Routes } from "react-router-dom";
 import PrivateRoute from "./components/PrivateRoute";
 import TaskManager from "./components/TaskManager";
 import Authentication from "./components/Authentication";
+import { useEffect } from "react";
+import { useAuthStore } from "./store/useAuthStore";
 
 const App = () => {
+  const { isLoggedIn } = useAuthStore();
+
+  useEffect(() => {
+    localStorage.setItem("isLoggedIn", JSON.stringify(isLoggedIn));
+  }, [isLoggedIn]);
+
   return (
     <div>
       <Routes>
