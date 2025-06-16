873cb16e26aeaafd8f19ba4e0ad038cb45a9e382
Kelsen23 <nikolozigoreziani@gmail.com>
2025-06-16 09:37:49 +0400

    Add "Delete All Tasks" button to TaskManager

--- src/components/TaskManager.tsx
+++ src/components/TaskManager.tsx
@@ -1,7 +1,8 @@
 import Nav from "./Nav";
 import Filters from "./Filters";
 import { HoverBorderGradient } from "./ui/hover-border-gradient";
-import { CirclePlus } from 'lucide-react';
+import { CirclePlus } from "lucide-react";
+import { Trash2 } from 'lucide-react';
 
 const TaskManager = () => {
   return (
@@ -9,14 +10,21 @@ const TaskManager = () => {
       <Nav />
       <div className="flex flex-col items-center">
         <Filters />
-        <HoverBorderGradient
-          containerClassName="rounded-full"
-          as="button"
-          className="cursor-pointer bg-white text-black flex items-center space-x-2"
-        >
-          <CirclePlus size={16} />
-          <span>Create New Task</span>
-        </HoverBorderGradient>
+        <div className="flex flex-row gap-4">
+          <HoverBorderGradient
+            containerClassName="rounded-full"
+            as="button"
+            className="cursor-pointer bg-white text-black flex items-center space-x-2"
+          >
+            <CirclePlus size={16} />
+            <span>Create New Task</span>
+          </HoverBorderGradient>
+
+          <button className="flex flex-row gap-2 items-center bg-red-500 px-4 py-2 rounded-full text-white shadow-sm cursor-pointer hover:bg-white hover:text-red-500 transition-all ease-in duration-150">
+            <Trash2 size={20} />
+            <span>Delete All Tasks</span>
+          </button>
+        </div>
       </div>
     </div>
   );
